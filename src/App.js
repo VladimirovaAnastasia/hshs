dc3544c98f278e5db55406ab486fc8eb897bb94f
vladimirova.a-a <vladimirovaaal@intech.rshb.ru>
2025-01-28 15:37:32 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={'test1'}>1</div>
+      <div className={'test2'}>2</div>
+    </div>
+  );
+}
+
+export default App;
